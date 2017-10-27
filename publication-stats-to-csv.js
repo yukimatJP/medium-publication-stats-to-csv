a662e39375b3f1f847793fa0c33698032acea82f
yukimatJP <tbonfukitama110@gmail.com>
2017-10-27 22:03:13 +0900

    remove jquery deferred

--- publication-stats-to-csv.js
+++ publication-stats-to-csv.js
@@ -1,3 +1,5 @@
+var separator = "-----------------------> ";
+
 function showInfo(msg) {
   console.log("[Info] " + msg);
 }
@@ -5,6 +7,64 @@ function showInfo(msg) {
 function async(f, delay) {
   setTimeout(f, delay);
 }
+
+function clickThreeMonthsButton() {
+  showInfo(separator + "START : Change to 90 days stats");
+
+  var threeMonthsBtn = $(".js-time-threeMonths button");
+  threeMonthsBtn.each(function() {
+    $(this).click();
+    console.log($(this));
+  });
+
+  showInfo(separator + "END : Change to 90 days stats");
+  return true;
+}
+
+
+function getGraphData() {
+  showInfo(separator + "START : Get graph data");
+
+  var convertRate = Array(3);
+  var statsData = Array(3);
+
+  var yAxis = $(".bargraph-yAxis");
+  var graphs = $(".js-barGraphBars");
+
+  for(var i=0; i<yAxis.length; i++) {
+
+    showInfo(separator + "loop: " + i);
+
+    // get graph height
+    var graphHeight = $(graphs[i]).get(0).getBBox().height;
+    showInfo("graphHeight : " + graphHeight);
+
+    // calculate convert rate
+    var maxScale = $(yAxis[i]).find("g").last();
+    var maxScaleVal = maxScale.find("text").text();
+    var maxScalePos = maxScale.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[5];
+    convertRate[i] = maxScaleVal / (graphHeight - maxScalePos);
+
+    showInfo("maxScaleVal : " + maxScaleVal);
+    showInfo("maxScalePos : " + maxScalePos);
+    showInfo("convertRate : " + convertRate[i]);
+
+    // calculate each graphBar value
+    var graphBarValues = Array();
+    var graphBars = $(graphs[i]).find("rect");
+    graphBars.each(function() {
+      graphBarValues.push($(this).attr("height") * convertRate[i]);
+    });
+    statsData[i] = graphBarValues;
+
+    showInfo(separator + "done");
+  }
+
+  showInfo(separator + "END : Get graph data");
+
+  return statsData;
+}
+
 
 (function() {
   // jQuery
@@ -16,73 +76,9 @@ function async(f, delay) {
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
   }
 
-  var clickThreeMonthsButton = function() {
-    showInfo("Start : Change to 90 days stats");
-
-    var threeMonthsBtn = $(".js-time-threeMonths button");
-
-    if(threeMonthsBtn.length == 3) {
-      showInfo("Find buttons! Change to 90 days stats.");
-      threeMonthsBtn.each(function() {
-        $(this).click();
-        console.log($(this));
-      });
-      showInfo("End : Change to 90 days stats");
-      return true;
-    } else {
-      showInfo("Could not find buttons... Try again.");
-      async(clickThreeMonthsButton, 1000);
-    }
-
-  }
-
-  var getGraphData = function() {
-    showInfo("Start : Get graph data");
-
-    var convertRate = Array(3);
-    var statsData = Array(3);
-
-    var yAxis = $(".bargraph-yAxis");
-    var graphs = $(".js-barGraphBars");
-
-    for(var i=0; i<yAxis.length; i++) {
-
-      // get graph height
-      var graphHeight = $(graphs[i]).get(0).getBBox().height;
-      showInfo("graphHeight : " + graphHeight);
-
-      // calculate convert rate
-      var maxScale = $(yAxis[i]).find("g").last();
-      var maxScaleVal = maxScale.find("text").text();
-      var maxScalePos = maxScale.css('transform').replace(/[^0-9\-.,]/g, '').split(',')[5];
-      convertRate[i] = maxScaleVal / (graphHeight - maxScalePos);
-
-      showInfo("maxScaleVal : " + maxScaleVal);
-      showInfo("maxScalePos : " + maxScalePos);
-      showInfo("convertRate : " + convertRate[i]);
-
-      // calculate each graphBar value
-      var graphBars = $(graphs[i]).find("rect");
-      graphBars.each(function() {
-        var graphBarHeight = $(this).attr("height") * convertRate[i];
-        console.log(graphBarHeight);
-      });
-    }
-
-    return "hogehoge";
-  }
-
   async(function() {
-    var d = $.Deferred();
-    d.promise()
-      .then(clickThreeMonthsButton()) // change to 90 days stats
-      .then(getGraphData())
-      .fail(function() {
-        console.log('common fail' + arguments);
-      })
-      .always(function() {
-        console.log('always' + arguments);
-      });
+    clickThreeMonthsButton();
+    getGraphData();
   }, 2000);
 
 }());
